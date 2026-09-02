31c9e2d4e1e81197eb03ebfe8a5fb930c0cec812
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-02 20:41:25 +0800

    Day001.java

---
+++ day001.java
@@ -0,0 +1,19 @@
+package day1;
+
+//nama class day1
+public class Day1 {
+ 
+    /*ini adalah method utama dalam java
+    tempat program kita untuk mulai dijalankan
+    */
+    public static void main(String[] args) {
+        System.out.println("nama saya mirwan");
+       
+        
+        /**dan yang terakhir adalah sout untuk menampilkan
+         * hasil dari yang kita program atau coding 
+         * 
+         */
+    }
+    
+}
