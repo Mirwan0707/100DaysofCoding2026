110828cc0d4d2d1b3e2e50559a6a3dde28d78a8f
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-03 20:29:37 +0800

    Day002.java

---
+++ Day002.java
@@ -0,0 +1,28 @@
+package Day002;
+
+public class Day002 {
+  
+public static void main(String[] args) {
+        
+        //penggunaan println();
+        System.out.println("BIODATA DIRI    : ");
+        System.out.println("Nama \t\t: Mirwan");
+        System.out.println("NIM  \t\t: D0225031");
+        System.out.println("Prodi \t\t: Informatika");
+        System.out.println("Jenis Kelamin \t: Laki-laki");
+        
+        //penggunaan print();
+        System.out.print("Umur \t\t: 20 Tahun");
+        
+        //penggunaan printf();(saya gunakan \n untuk membuat baris baru
+        //karena diatas penggunaan print makanya saya tambahkan \n biar tidak tersambung
+        System.out.printf("\nhoby \t\t: VollyBall");
+        System.out.println("");
+        
+       /*catatan : itu \t untuk membuat tab dan merapikan 
+        teks pada titik duanya.
+        
+        */
+    }
+    
+}
