a865b8b98537445f2dc527858e657fdc4b623d0f
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-05 03:13:26 +0800

    Day004.java

---
+++ Day004.java
@@ -0,0 +1,21 @@
+
+package day004;
+
+
+public class Day004 {
+
+    public static void main(String[] args) {
+        byte umur = 20;
+        short tahun = 2006;
+         int Mahasiswa = 900000;
+        long jarak = 2000000000L;
+        
+        System.out.println("saya berumur "+ umur + " tahun");
+        System.out.println("lahir tahun "+ tahun);
+        System.out.println("Jumlah Mahasiswa : "+ Mahasiswa);
+        System.out.println("jarak : " + jarak);
+        
+        
+    }
+    
+}
