911509443c0f16adb210dfd04a040cdb4d15ac76
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-04 22:08:16 +0800

    Day003.java

---
+++ Day003.java
@@ -0,0 +1,35 @@
+
+package day003;
+
+
+public class Day003 {
+
+  
+    public static void main(String[] args) {
+         // \n = pindah baris
+        System.out.println("Nama: Mirwan\nNIM: 123456");
+
+        // \t = membuat jarak / tab
+        System.out.println("Nama\t: Mirwan");
+        System.out.println("NIM\t: 123456");
+
+        // \" = menampilkan tanda petik dua
+        System.out.println("Dia berkata: \"Halo\"");
+
+        // \' = menampilkan tanda petik satu
+        System.out.println("Huruf \'A\'");
+
+        // \\ = menampilkan tanda backslash
+        System.out.println("Folder: C:\\Java\\Project");
+
+        // \b = backspace
+        System.out.println("ABC\bD");
+
+        // \r = kembali ke awal baris
+        System.out.println("Halo\rHi");
+
+        // \f = form feed
+        System.out.println("Halo\fDunia");
+    }
+    
+}
