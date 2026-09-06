ae411dd6cfaad666d272fc77cb67869973f87eb5
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-07 00:07:39 +0800

    Day006.java

---
+++ Day006.java
@@ -0,0 +1,12 @@
+package day006;
+
+public class Day006 {
+
+    public static void main(String[] args) {
+       char InisialNama = 'M';
+       boolean BenarSalah = true;
+        System.out.println("Inisial nama Mirwan Adalah  "+ InisialNama);
+        System.out.println("apakah benar atau salah : "+ BenarSalah+("(benar)"));
+    }
+    
+}
