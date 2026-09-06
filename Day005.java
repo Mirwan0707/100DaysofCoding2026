d8fa92e1d43ad1c18e5f5ec34ae44a3e9797645a
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-06 11:58:09 +0800

    Day005.java

---
+++ Day005.java
@@ -0,0 +1,14 @@
+package day005;
+
+public class Day005 {
+
+   
+    public static void main(String[] args) {
+        double BB= 55.50;
+        float  TB = 150.0f;
+                
+          System.out.println("Berat Badan : "+ BB + "kg");
+          System.out.println("Tinggi Badan : "+ TB + "cm");
+    }
+    
+}
