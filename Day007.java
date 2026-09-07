f0a4541771fb90b8050e6235f52941d462c9a5f4
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-08 00:32:32 +0800

    Day007.java

---
+++ Day007.java
@@ -0,0 +1,14 @@
+
+package pkg100dayscoding;
+
+
+public class day07{
+    public static void main(String[] args) {
+        String nama = "Mirwan";
+       
+        System.out.println("nama saya adalah "+ nama);
+    }
+
+
+}
+    
