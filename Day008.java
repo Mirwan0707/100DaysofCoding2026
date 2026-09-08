71c755de443a40cc29e98520b5e44feb0d570c43
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-09 02:21:15 +0800

    Day008.java

---
+++ Day008.java
@@ -0,0 +1,15 @@
+package pkg100dayscoding;
+
+public class day08 {
+        public static void main(String[] args) {
+        int umur; //deklarasi
+        
+               umur = 19; //inisialisasi
+               System.out.println("umur awal : "+ umur);
+               
+               umur = 20; //update
+               System.out.println("umur sekarang : "+ umur);
+    }
+
+
+}
