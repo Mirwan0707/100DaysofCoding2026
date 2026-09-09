0364906883240829640a6d05985eb8da6c3d98e5
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-10 00:34:31 +0800

    Day009.java

---
+++ Day009.java
@@ -0,0 +1,16 @@
+
+package pkg100dayscoding;
+
+public class day009 {
+    public static void main(String[] args) {
+      
+        final String nama = "Mirwan";
+        final double tb = 160.0;
+        final int kelahiran = 2026;
+          
+        System.out.print("Nama saya "+ nama+",");
+        System.out.print("Tingggi badan saya "+ tb+"cm, ");
+        System.out.println("Kelahiran "+ kelahiran+".");
+                
+    }
+}
