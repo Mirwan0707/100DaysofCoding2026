e30929382bd3e591f3c57554caf9eda58f4f89b2
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-12 19:53:58 +0800

    Day011.java

---
+++ Day011.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding;
+
+import java.util.Scanner;
+public class Day011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nama anda : ");
+        String nama = sc.nextLine();
+        
+        System.out.println("nama saya "+ nama);
+    }
+}
