38951ebedb5f15f6ed5e2d2947aa6ae9b2d03896
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-15 19:57:34 +0800

    Day014.java

---
+++ Day014.java
@@ -0,0 +1,20 @@
+package pkg100dayscoding;
+
+
+public class Day014 {
+    public static void main(String[] args) {
+     
+        int a = 20;
+       int b = 10;
+       
+       int penjumlahan = a+b;
+       int pengurangan = a-b;
+        System.out.println("hasil penjumlahan : "+ penjumlahan);
+        System.out.println("hasil pengurangan : "+ pengurangan);
+     
+
+     
+
+    }
+   
+}
