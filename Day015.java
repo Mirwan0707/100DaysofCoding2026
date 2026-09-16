7dcdced270b74b78b968df19a8d79e565c3b433c
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-16 18:46:44 +0800

    Day015.java

---
+++ Day015.java
@@ -0,0 +1,18 @@
+
+package pkg100dayscoding;
+
+public class Day15 {
+    public static void main(String[] args) {
+
+       int a = 20;
+       int b = 10;
+       
+       int perkalian = a*b;
+       int pembagian= a/b;
+      
+        System.out.println("hasil perkalian : "+ perkalian);
+        System.out.println("hasil pembagian : "+ pembagian);
+     
+     
+    }
+}
