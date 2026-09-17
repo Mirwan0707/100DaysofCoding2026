3f1945140f660242e32dc896da7661e4001d01c8
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-17 21:09:15 +0800

    Day016.java

---
+++ Day016.java
@@ -0,0 +1,16 @@
+    package pkg100dayscoding;
+
+
+       public class Day016 {
+            public static void main(String[] args) {
+         
+            int a = 27;
+            int b = 8;
+       
+            int modulus = a%b;
+      
+       //modulus  itu sisa dari hasil pembagian
+        System.out.println("hasil sisa bagi : "+ modulus);
+        
+         }
+}
