aaa79c08e245937a8079c089d3aa88fd4a59fe8b
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-19 19:39:19 +0800

    Day018.java

---
+++ Day018.java
@@ -0,0 +1,9 @@
+public class Day18 {
+    public static void main(String[] args) {
+        byte angka = 25;
+        long hasil = angka;
+        
+        System.out.println("nilai awal int : "+ angka);
+        System.out.println("hasil dari konversi  : "+ hasil);
+    }
+}
