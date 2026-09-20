8f18a3e32320edb1f4e72027ee2f9db88dd9debd
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-20 22:11:49 +0800

    Day019.java

---
+++ Day019.java
@@ -0,0 +1,10 @@
+public class Day019 {
+            public static void main(String[] args) {
+         
+        long angka = 100;
+        byte hasil = (byte) angka;
+
+        System.out.println(hasil);
+        
+         }
+}
