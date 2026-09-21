41ecdcea9bf543bee2af30a515f3c468203c4067
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-21 18:21:19 +0800

    Day020.java

---
+++ Day020.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day020 {
+    public static void main(String[] args) {
+     
+        int  angka = 200;
+        
+              String hasil = String.valueOf(angka);
+             System.out.println("hasil tipe data int ke String : "+ angka);
+  
+        
+        
+    }
+}
