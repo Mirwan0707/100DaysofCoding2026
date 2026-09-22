ab3610023da70a738eb0621a42ff705eea89794d
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-22 22:46:26 +0800

    Day021.java

--- Day021.java
+++ Day021.java
@@ -1,13 +1,32 @@
 public class Day021 {
     public static void main(String[] args) {
 
-        String teksInt = "200";
-        String teksByte = "100";
+        String teksByte = "10";
+        String teksShort = "1000";
+        String teksInt = "20000";
+        String teksLong = "100000";
+        String teksFloat = "10.5";
+        String teksDouble = "20.5";
+        String teksChar = "A";
+        String teksBoolean = "true";
 
-        int angkaInt = Integer.parseInt(teksInt);
         byte angkaByte = Byte.parseByte(teksByte);
+        short angkaShort = Short.parseShort(teksShort);
+        int angkaInt = Integer.parseInt(teksInt);
+        long angkaLong = Long.parseLong(teksLong);
+        float angkaFloat = Float.parseFloat(teksFloat);
+        double angkaDouble = Double.parseDouble(teksDouble);
+        char angkaChar = teksChar.charAt(0);
+        boolean angkaBoolean = Boolean.parseBoolean(teksBoolean);
 
-        System.out.println("Hasil int: " + angkaInt);
-        System.out.println("Hasil byte: " + angkaByte);
+        System.out.println("Byte: " + angkaByte);
+        System.out.println("Short: " + angkaShort);
+        System.out.println("Int: " + angkaInt);
+        System.out.println("Long: " + angkaLong);
+        System.out.println("Float: " + angkaFloat);
+        System.out.println("Double: " + angkaDouble);
+        System.out.println("Char: " + angkaChar);
+        System.out.println("Boolean: " + angkaBoolean);
+        
     }
 }
