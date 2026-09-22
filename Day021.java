1587a69fee07460acbcc63f3850aaacae2cc3f81
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-22 18:44:59 +0800

    Day021.java

---
+++ Day021.java
@@ -0,0 +1,13 @@
+public class Day021 {
+    public static void main(String[] args) {
+
+        String teksInt = "200";
+        String teksByte = "100";
+
+        int angkaInt = Integer.parseInt(teksInt);
+        byte angkaByte = Byte.parseByte(teksByte);
+
+        System.out.println("Hasil int: " + angkaInt);
+        System.out.println("Hasil byte: " + angkaByte);
+    }
+}
