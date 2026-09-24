32b45337946d37454586513521341b96b68d7619
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-24 21:03:27 +0800

    Day023.java

---
+++ Day023.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day023 {
+
+    public static void main(String[] args) {
+
+        int sisi = 10;
+        int luas = sisi * sisi;
+
+        System.out.println("=== Menghitung Luas Persegi ===");
+        System.out.println("Sisi : " + sisi);
+        System.out.println("Luas : " + luas);
+    }
+}
