219a9381da2c6377481935abfba44ddac272724f
Mirwan0707 <mirwanirwan416@gmail.com>
2026-09-26 15:48:36 +0800

    Day025.java

---
+++ Day025.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding;
+
+public class Day025 {
+
+    public static void main(String[] args) {
+        
+        int jariJari = 7;
+        double luas = 3.14 * jariJari * jariJari;
+        
+        System.out.println("Jari-jari = " + jariJari);
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
